2f73413da06237134a307ec49d3285b0c01e3703
Leonardo Vieira <leonardojcv@Outlook.com>
2023-12-20 22:13:39 -0300

    Zuul configuration complete

---
+++ api-gateway-zuul/src/main/java/com/leonardojcv/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,16 @@
+package com.leonardojcv.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ api-gateway-zuul/src/test/java/com/opdevinit/gamesgatewayzuul/GamesGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opdevinit.gamesgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
